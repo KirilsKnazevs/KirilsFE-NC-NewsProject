fbee4cfc650ed436b438a54a8401b31a66c40b5b
Kirils <kirils.knazevs@gmail.com>
2022-11-03 14:11:05 +0000

    add functionality to downvote article

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 function Votes({ votes, article_id }) {
   const [upVote, setUpVote] = useState(0);
+  const [downVote, setDownVote] = useState(0);
   const upVotesOnApi = { inc_votes: 1 };
   const downVotesOnApi = { inc_votes: -1 };
 
@@ -20,27 +21,25 @@ function Votes({ votes, article_id }) {
   }
 
   function handleDownVotes() {
-    if (upVote !== 0) {
-      setUpVote((currentVotes) => currentVotes - 1);
-      fetch(`https://kirils-ncnews.herokuapp.com/api/articles/${article_id}`, {
-        method: "PATCH",
-        headers: { "Content-type": "application/json" },
-        body: JSON.stringify(downVotesOnApi),
-      })
-        .then((res) => res.json())
-        .catch((error) => {
-          console.error("Error:", error);
-        });
-    }
+    setDownVote((currentVotes) => currentVotes - 1);
+    fetch(`https://kirils-ncnews.herokuapp.com/api/articles/${article_id}`, {
+      method: "PATCH",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(downVotesOnApi),
+    })
+      .then((res) => res.json())
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   }
 
   return (
     <div>
-      <p>Total Votes: {votes + upVote}</p>
+      <p>Total Votes: {votes + upVote + downVote}</p>
       <button disabled={upVote !== 0} onClick={handleUpVotes}>
         Upvote ⬆
       </button>
-      <button disabled={upVote === 0} onClick={handleDownVotes}>
+      <button disabled={downVote !== 0} onClick={handleDownVotes}>
         Downvote ⬇
       </button>
     </div>
